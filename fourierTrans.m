e1b291173b4f1762c81d886f42ce96e039c77218
Ivan1Kulikov <167446804+Ivan1Kulikov@users.noreply.github.com>
2024-04-30 00:27:49 +0300

    Add files via upload

---
+++ fourierTrans.m
@@ -0,0 +1,39 @@
+function [Sp,T,F]=fourierTrans(signal,max_freq)
+Sp=signal;
+T=1:length(signal);
+F=0:10^6:max_freq;
+Sp=zeros(length(T),length(F));%Т строк F столбцов (при движении по строке меняется частота)
+temp=0;
+stepF=1; %нужен для формирования таблицы по порядковм номерам
+
+for f=F
+    Sp(1,stepF)=signal(1)*exp(-1j*2*pi*f*1);
+    stepF=stepF+1; 
+end
+%
+% for t=1:length(signal) %движение по строкам (по времени)
+%     stepF=1;
+% 
+%     for f=0:10^7:max_freq %движение по столбцам (по частотам на конкретном времени)
+%         tempSignal=signal(t)*exp(-1j*f*2*pi*t);%спектр в момент t на частоте w
+% 
+%         for sumT=1:t 
+%            tempSp=Sp(sumT,stepF);
+%            Sum= 
+%         end
+%          stepF=stepF+1;
+%          Sp(t,stepF)=tempSignal;
+% end
+stepF=1;
+for f=F %движение по столбцам (по частотам на конкретном времени)
+    
+    for t=2:length(signal)
+        tempSignal=signal(t)*exp(-1j*2*pi*f*t*10^-10)+Sp(t-1,stepF);
+        Sp(t,stepF)=tempSignal;
+    end
+
+    stepF=stepF+1;
+end
+
+end
+
---
+++ spectrogramma.m
@@ -0,0 +1,30 @@
+clear, clc, close all
+time_array=10^-5;%суммарная длительность сигнала
+time_step=4*10^-9/3;%дискретизация сигнала
+freq_step=1/time_step;
+freq=10^8;
+signal=[];
+
+%function [signal, NumberPeriods,time_point, numberPoints]=sin_gen(Ampl,freq,Time_array,phase,time_begin,time_end,step
+[signal1,NumberPeriods,time_point,numberPoints1]=sin_gen(10,freq,time_array,0,0.7*10^-5,0.9*10^-5,time_step);
+signal=[signal,signal1];
+
+for i=2:8
+    [new_signal]=sin_gen(10,freq*i,time_array,0,0.2*10^-5,0.3*10^-5,time_step);
+    signal=signal+new_signal;
+end
+
+figure(1); plot(time_point,signal);
+
+A=fft(signal,length(signal));
+% A=fftshift(A);
+ figure(2); plot(abs(A));
+
+ [Sp,F,T]= spectrogram(signal,rectwin(numberPoints1/30*5),0,1000,freq);
+ figure(3);contourf(F,T,abs(Sp'));
+
+ figure(4);spectrogram(signal,rectwin(1000),0,1000,freq);
+ 
+%function [Sp,T,F]=fourierTrans(signal,max_freq)
+[Sp,T,F]=fourierTrans(signal,freq*5);
+figure(5);contourf(T,F,abs(Sp'));
