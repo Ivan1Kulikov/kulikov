2bd686c05f1daaceb848a94a683532dbc73df416
Ivan1Kulikov <167446804+Ivan1Kulikov@users.noreply.github.com>
2024-04-30 00:39:02 +0300

    spectrFT.m

--- spectrFT.m
+++ spectrFT.m
@@ -1,15 +1,1 @@
-function [C_Amplitude,F]=spectrFT(signal,freq_step)
 
-w=0:1000*pi/freq_step:pi;
-F=freq_step*w/(2*pi);
-C_Amplitude=zeros(length(signal),length(signal));
-
-for m=1:length(signal)
-    Cmk=0;
-    for k=1:length(signal)
-        X=signal(1,k)*exp(-1j*2*pi*m*(k-1)/(length(signal)) );
-        Cmk=Cmk+X;
-        C_Amplitude(m,k)=Cmk;
-    end  
-     C_Amplitude(m,k)=Cmk;
-end
