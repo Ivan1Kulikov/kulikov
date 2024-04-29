5d318a1ae9dbdeacaaf3a66dfbc6663280be0ab7
Ivan1Kulikov <167446804+Ivan1Kulikov@users.noreply.github.com>
2024-04-30 02:28:51 +0300

    fourierTrans.m

--- fourierTrans.m
+++ fourierTrans.m
@@ -10,20 +10,7 @@ for f=F
     Sp(1,stepF)=signal(1)*exp(-1j*2*pi*f*1);
     stepF=stepF+1; 
 end
-%
-% for t=1:length(signal) %движение по строкам (по времени)
-%     stepF=1;
-% 
-%     for f=0:10^7:max_freq %движение по столбцам (по частотам на конкретном времени)
-%         tempSignal=signal(t)*exp(-1j*f*2*pi*t);%спектр в момент t на частоте w
-% 
-%         for sumT=1:t 
-%            tempSp=Sp(sumT,stepF);
-%            Sum= 
-%         end
-%          stepF=stepF+1;
-%          Sp(t,stepF)=tempSignal;
-% end
+
 stepF=1;
 for f=F %движение по столбцам (по частотам на конкретном времени)
     
